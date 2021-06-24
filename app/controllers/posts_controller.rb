b6f9c8ca03ce255bad5d4c28da80be59c360bbc9
Teddy-Livingstone Ememandu <livistteddy@gmail.com>
2021-06-24 19:33:37 +0100

    Implement requested changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,6 +21,8 @@ class PostsController < ApplicationController
   # POST /posts or /posts.json
   def create
     @post = current_user.posts.build(post_params)
+    puts "--------------"
+    puts @post
 
     respond_to do |format|
       if @post.save
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :posts , only: [:create, :new, :index , :show]
-  devise_for :users
+  resources :posts , only: [:create, :new, :index]
+  devise_for :users, :controllers => { registrations: 'registrations' }
   root  'posts#index'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
